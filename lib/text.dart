ecc6214fc3b78d76c75481f5aef98ce0e2438e54
ay-dex <megha.dexbytes@gmail.com>
2024-04-04 15:35:19 +0530

    ui

---
+++ lib/text.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Textjdfh extends StatefulWidget {
+  const Textjdfh({super.key});
+
+  @override
+  State<Textjdfh> createState() => _TextjdfhState();
+}
+
+class _TextjdfhState extends State<Textjdfh> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
